24836b0c4e9d0bae49c12b5be92e7309ef8cabce
Aaron <Aaron>
2021-12-13 13:22:48 +0100

    primer commit

---
+++ src/Debugger.java
@@ -0,0 +1,20 @@
+
+public class Debugger {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+		
+		System.out.print("Dame un numero: ");
+		int num = Console.readInt();
+		int n_factorial = factorial(num);
+	}
+	
+	public static int factorial(int num){
+		int fact = 1;
+		for(int i=1;i<=num;i++) {
+			fact = fact * i;
+		}
+		return fact;
+	}
+}
